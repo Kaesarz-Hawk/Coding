6f6ff87496507f6f3be654cbc21a5d1ac1f929de
Kaesarz-Hawk <rokeykid777@gmail.com>
2026-05-13 21:13:25 +0600

    Auto update

--- Codechef/Contests/START238D/problem1.cpp
+++ Codechef/Contests/START238D/problem1.cpp
@@ -7,7 +7,24 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int x, y,z;
+    cin>>x>>y>>z;
+    int aus_total_score = x+z;
+
+    if ( aus_total_score > y )
+    {
+        cout << (aus_total_score - y)+1 << endl;
+    }
+    else if ( aus_total_score == y )
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
     
+
 
     return 0;
 }
--- Codechef/Contests/START238D/problem2.cpp
+++ Codechef/Contests/START238D/problem2.cpp
@@ -1,11 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
+
+        if (x <= 20)
+        {
+            cout << x * 10 << endl;
+        }
+        else
+        {
+            int first_phase_cost = 20 * 10; 
+            int remaining = x - 20;
+            int balls_lagbo = (remaining + 1) / 2; 
+            int second_phase_cost = balls_lagbo * 5;
+
+            cout << first_phase_cost + second_phase_cost << endl;
+        }
+    }
 
     return 0;
 }
