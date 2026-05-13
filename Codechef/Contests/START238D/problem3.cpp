b9305082a01e6b5f6e4e661bb0f7d1785bc158d3
Kaesarz-Hawk <rokeykid777@gmail.com>
2026-05-13 21:27:27 +0600

    Auto update

--- Codechef/Contests/START238D/problem3.cpp
+++ Codechef/Contests/START238D/problem3.cpp
@@ -7,5 +7,38 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N_particpants, X_selectable, K_topeligible;
+        cin >> N_particpants >> X_selectable >> K_topeligible;
+
+        vector<int> scores(N_particpants);
+        for (int i = 0; i < N_particpants; i++)
+        {
+            cin >> scores[i];
+        }
+
+        sort(scores.begin(), scores.end(), greater<int>());
+
+        int eligible = 0;
+        int clusters = 0;
+
+        for (int i = 0; i < N_particpants; i++)
+        {
+
+            if (i == 0 or scores[i] != scores[i - 1])
+            {
+                if (clusters == K_topeligible)
+                    break;
+                clusters++;
+            }
+            eligible++;
+        }
+
+        cout << min(eligible, X_selectable) << endl;
+    }
+
     return 0;
 }
