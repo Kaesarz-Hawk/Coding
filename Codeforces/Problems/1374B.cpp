dec57294af4097dc1c9408f8f9a30725c844488d
Kaesarz-Hawk <rokeykid777@gmail.com>
2026-05-27 03:16:47 +0600

    Auto update

---
+++ Codeforces/Problems/1374B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define SpeedxKH                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+    void div6(ll &n, int &moves_count)
+    {
+        n /= 6;
+        moves_count++;
+    }
+    void mul2(ll &n, int &moves_count)
+    {
+        n *= 2;
+        moves_count++;
+    }
+
+int main()
+{
+    SpeedxKH;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    ll n;
+    cin >> n;
+
+    int moves_count = 0;
+
+    while (n != 1)
+    {
+        if (n % 6 == 0)
+        {
+            div6(n, moves_count);
+        }
+        else if (n % 3 == 0)
+        {
+            mul2(n, moves_count);
+        }
+        else
+        {
+            moves_count = -1;
+            break;
+        }
+    }
+
+    cout << moves_count << endl;
+   
+}
+    return 0;
+}
---
+++ Codeforces/Problems/1789A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define SpeedxKH ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
+        int remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    return a;
+}
+
+int main()
+{
+    SpeedxKH;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        bool found = false;
+
+        for (int i = 0; i < n and !found; i++)
+        {
+            for (int j = i + 1; j < n and !found; j++)
+            {
+                if (gcd(a[i], a[j]) <= 2)
+                {
+                    found = true;
+                }
+            }
+        }
+        found ? cout << "YES" << endl : cout << "NO" << endl;
+    }
+    return 0;
+}
