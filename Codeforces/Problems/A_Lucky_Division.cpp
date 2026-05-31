c846de845067c86515726ba9739036f34e47f30e
Kaesarz-Hawk <rokeykid777@gmail.com>
2026-05-31 21:15:45 +0600

    Auto update

--- Codeforces/Problems/A_Lucky_Division.cpp
+++ Codeforces/Problems/A_Lucky_Division.cpp
@@ -1,36 +1,42 @@
 // Author  : Kaesarz
 // Date    : 31-05-2026
-// Time    : 19:31
+// Time    : 18:55
 #include <bits/stdc++.h>
 using namespace std;
 #define ll      long long
 #define SpeedxKH ios_base::sync_with_stdio(false); cin.tie(NULL);
+
 const int MOD  = 1e9 + 7;
 const int INF  = 1e9;
 const ll  LLINF = 1e18;
 
-void solve()
-{
-    long long n_naturalnumbers, k_position;
-    cin >> n_naturalnumbers >> k_position;
-
-    long long count_odds = (n_naturalnumbers + 1) / 2;
-
-    long long answer;
-    if (k_position <= count_odds)
-        answer = 2 * k_position - 1;          
-    else
-        answer = 2 * (k_position - count_odds); 
-
-    cout << answer << '\n';
+void solve() {
+    int n;
+     cin >> n;
+   bool isLucky = false;
+   vector<int> luckyNumbers = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+   for (int lucky : luckyNumbers) {
+       if (n % lucky == 0) {
+           isLucky = true;
+           break;
+       }
+   }
+    if (isLucky) {
+         cout << "YES\n";
+    } else {
+         cout << "NO\n";
+    }
 }
-   
+
 int main() {
     SpeedxKH
+
     // int t;
     // cin >> t;
     // while (t--){ solve(); }
+
     // single test case
     solve();
+
     return 0;
 }
--- Codeforces/Problems/A_Theatre_Square.cpp
+++ Codeforces/Problems/A_Theatre_Square.cpp
@@ -1,36 +1,37 @@
 // Author  : Kaesarz
 // Date    : 31-05-2026
-// Time    : 19:31
+// Time    : 19:02
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll      long long
 #define SpeedxKH ios_base::sync_with_stdio(false); cin.tie(NULL);
+
 const int MOD  = 1e9 + 7;
 const int INF  = 1e9;
 const ll  LLINF = 1e18;
 
-void solve()
-{
-    long long n_naturalnumbers, k_position;
-    cin >> n_naturalnumbers >> k_position;
+void solve() {
+  
+    ll n, m, a;
+        cin >> n >> m >> a;
 
-    long long count_odds = (n_naturalnumbers + 1) / 2;
+    ll flagstonesAlongLength = (n + a - 1) / a; 
+    ll flagstonesAlongWidth = (m + a - 1) / a;
 
-    long long answer;
-    if (k_position <= count_odds)
-        answer = 2 * k_position - 1;          
-    else
-        answer = 2 * (k_position - count_odds); 
-
-    cout << answer << '\n';
+    ll totalFlagstones = flagstonesAlongLength * flagstonesAlongWidth;
+    cout << totalFlagstones << "\n";
 }
-   
+
 int main() {
     SpeedxKH
+
     // int t;
     // cin >> t;
     // while (t--){ solve(); }
+
     // single test case
     solve();
+
     return 0;
 }
