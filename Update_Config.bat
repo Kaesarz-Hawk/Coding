cd33553e9a04d7a3f108546ab1af9fa987321d8d
Kawsar Hoque <rokeykid777@gmail.com>
2026-08-14 19:23:30 +0600

    Auto update

---
+++ Update_Config.bat
@@ -0,0 +1,20 @@
+@echo off
+title Coding Repository Updater
+
+cd /d "C:\Users\Kawsar Hoque\Desktop\Coding"
+
+echo.
+echo ==========================================
+echo       Checking GitHub for updates...
+echo ==========================================
+echo.
+
+git pull
+
+echo.
+echo ==========================================
+echo                Finished
+echo ==========================================
+echo.
+
+pause
