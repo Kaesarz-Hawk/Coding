f4d50eda04e404ddb9865a7565f6813e4d912fc9
Kaesarz-Hawk <rokeykid777@gmail.com>
2026-09-04 10:25:36 +0600

    Auto update

---
+++ LogicBuilding with loops/Nested loops/MULTIPICATIONTABLE.cpp
@@ -0,0 +1,37 @@
+// Author  : Kaesarz
+// Date    : 02-09-2026
+// Time    : 12:28
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define SpeedxKH ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LLINF = 1e18;
+
+void solve() {
+    int n = 10;
+    for(int i = 1; i <= 10; i++){
+        for( int j = 1 ; j <=10 ; j++){
+            cout << j << " x " << n <<" = "<<" "<< j*n << endl;
+            
+        }
+        break;
+    }
+}
+
+int main() {
+    SpeedxKH;
+
+    // int t;
+    // cin >> t;
+    // while (t--) solve();
+
+    // single test case
+    solve();
+
+    return 0;
+}
